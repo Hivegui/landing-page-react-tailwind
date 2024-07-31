edbb6aacfaa329e95250339f2766e0c78d3e1256
Guilherme Gomes <guifariasgomes@gmail.com>
2024-07-31 09:54:26 -0300

    att 2.0

--- src/App/Page/HomePage.tsx
+++ src/App/Page/HomePage.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import { Carousel } from "react-responsive-carousel";
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // Importe o CSS do Carousel aqui
 
